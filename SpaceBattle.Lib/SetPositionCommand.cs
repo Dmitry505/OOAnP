f57719e23a6cf59f88fb021926a7966e87560fc8
Dmitry505 <dimkad1810@mal.ru>
2025-03-02 16:12:41 +0600

    feat: class SetPositionCommand

---
+++ SpaceBattle.Lib/SetPositionCommand.cs
@@ -0,0 +1,15 @@
+﻿public class SetPositionCommand : ICommand
+{
+    private readonly List<Vector> _positions;
+    public SetPositionCommand(List<Vector> PositionList) => _positions = PositionList;
+
+    public void Execute()
+    {
+        var ships = IoC.Resolve<List<IUObject>>("Game.Ships.All");
+
+        for (var i = 0; i < ships.Count; i++)
+        {
+            ships[i].SetProperty("Position", _positions[i]);
+        }
+    }
+}
