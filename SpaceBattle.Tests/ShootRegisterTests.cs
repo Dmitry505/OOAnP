b8b73f4ec9e3169a997a39cb2de9cb4df74e5489
Dmitry505 <dimkad1810@mal.ru>
2025-03-16 23:00:23 +0600

    modified: ShootRegisterTests add checking for incorrect values

--- SpaceBattle.Tests/ShootRegisterTests.cs
+++ SpaceBattle.Tests/ShootRegisterTests.cs
@@ -32,4 +32,34 @@ public class ShootRegisterTests
         testShot.Verify(m => m.SetProperty("Angle", 45), Times.Once);
         testShot.Verify(m => m.SetProperty("Speed", 10), Times.Once);
     }
+
+    [Fact]
+    public void CreatingShotMissingShipReturnNullReferenceException()
+    {
+
+        var testShot = new Mock<IUObject>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Object.Register", (object[] args) => testShot.Object).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Object.Shot", (object[] args) =>
+        { return new ShootRegister((IUObject)args[0]); }).Execute();
+
+        Assert.Throws<NullReferenceException>(() => IoC.Resolve<ShootRegister>("Game.Object.Shot", null!).Shot());
+    }
+
+    [Fact]
+    public void CreatingShotShipNullAngleReturnNullReferenceException()
+    {
+
+        var ships1 = new Mock<IUObject>();
+        ships1.Setup(m => m.GetProperty("Position")).Returns((10, 20));
+        ships1.Setup(m => m.GetProperty("Angle")).Throws(new ArgumentNullException());
+
+        var testShot = new Mock<IUObject>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Object.Register", (object[] args) => testShot.Object).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Object.Shot", (object[] args) =>
+        { return new ShootRegister((IUObject)args[0]); }).Execute();
+
+        Assert.Throws<ArgumentNullException>(() => IoC.Resolve<ShootRegister>("Game.Object.Shot", ships1.Object).Shot());
+    }
 }
