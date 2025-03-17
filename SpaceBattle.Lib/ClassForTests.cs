c34e1038a30124649282c3673eb4c496e2767d0a
Dmitry505 <dimkad1810@mal.ru>
2025-03-17 15:46:52 +0600

    test

---
+++ SpaceBattle.Lib/ClassForTests.cs
@@ -0,0 +1,12 @@
+﻿public class ClassForTests
+{
+    public readonly int Position;
+    public readonly IRotateble Rotable;
+    public readonly string Name;
+    public ClassForTests(IRotateble Rotable, int Position, string Name)
+    {
+        this.Rotable = Rotable;
+        this.Position = Position;
+        this.Name = Name;
+    }
+}
