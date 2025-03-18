5baf282d991f7a4b7644329b7030c2d9f787ba89
Dmitry505 <dimkad1810@mal.ru>
2025-03-18 19:33:01 +0600

    feat: add SpaceBattle.Lib

---
+++ SpaceBattle.Lib/DiStrategy.cs
@@ -0,0 +1,14 @@
+﻿global using Hwdtech;
+
+public class DiStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var structure = (Type)args[0];
+        var parameters = structure.GetConstructors()[0].GetParameters();
+
+        var property = parameters.Select(m => IoC.Resolve<object>($"Addiction{m.ParameterType}"));
+
+        return Activator.CreateInstance(structure, property.ToArray())!;
+    }
+}
